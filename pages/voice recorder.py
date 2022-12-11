0f0cd2737f4a08a6007d8964de94d2f5498ca5da
Gowtham <75899822+gowtham-source@users.noreply.github.com>
2022-12-11 08:05:37 +0530

    Update voice recorder.py

--- pages/voice recorder.py
+++ pages/voice recorder.py
@@ -26,7 +26,7 @@ def audiorec_demo_app():
 
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     # Custom REACT-based component for recording client audio in browser
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
+    build_dir = os.path.join(parent_dir, "./st_audiorec/frontend/build")
     # specify directory and initialize st_audiorec object functionality
     st_audiorec = components.declare_component("st_audiorec", path=build_dir)
 
